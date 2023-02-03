7217e724d29c407464a066cfbef0adb29b31bea0
yago <yago@arpiatecnologia.com.br>
2023-02-03 01:55:39 -0300

    CRUD Cliente finalizado

--- Atividade2_POO/Cliente.cs
+++ Atividade2_POO/Cliente.cs
@@ -10,127 +10,23 @@ namespace Atividade2_POO
     {
         public int Id;
         public string Nome;
-        public string Ende;
-        public string DataNasci;
-
-
-        public Cliente(int id, string nome, string ende, string dataNasci)
-        {
-            Id = id;
-            Nome = nome;
-            Ende = ende;
-            DataNasci = dataNasci;
-
-        }
+        public string Endereco;
+        public string DataNascimento;
 
         public Cliente()
         {
 
         }
 
-        /*public void Menu()
-        {
-            int opcao = 0;
-
-            Console.WriteLine("  Por favor, selecione uma opção!");
-            Console.WriteLine("-----------------");
-            Console.WriteLine("| 1- Criar      |");
-            Console.WriteLine("| 2- Editar     |");
-            Console.WriteLine("| 3- Listar     |");
-            Console.WriteLine("| 4- Excluir    |");
-            Console.WriteLine("-----------------");
-
-            switch (opcao)
-            {
-                case 1:
-                    this.Criar();
-                    break;
-                //case 2:
-                //    this.Editar();
-                //    break;
-                //case 3:
-                //    this.Listar();
-                //    break;
-                //case 4:
-                //    this.Excluir();
-                //    break;
-                default:
-                    Console.WriteLine("Opção inválida!");
-                    break;
-            }
-        }*/
-
-        public void Criar()
-        {
-
-            Console.Write("Por favor, nos informe os seguintes dados:");
-            Console.Write("Nome: ");
-            string nome = Console.ReadLine();
-            Console.Write("Endereço: ");
-            string ende = Console.ReadLine();
-            Console.Write("Data de nascimento: ");
-            string dataNasci = Console.ReadLine();
-
-            this.Nome = nome;
-            this.Ende = ende;
-            this.DataNasci = dataNasci;
-
-        }
-
-        public void Editar()
-        {
-            Console.Write("Por favor, digite qual dado deseja ediar: ");
-            string opcao = Console.ReadLine();
-
-            switch (opcao)
-            {
-                case "nome":
-                    Console.Write("Digite o novo nome: ");
-                    string nome = Console.ReadLine();
-                    this.Nome = nome;
-                    break;
-                case "endereço":
-                    Console.Write("Digite o novo endereço: ");
-                    string ende = Console.ReadLine();
-                    this.Ende = ende;
-                    break;
-                case "data de nascimento":
-                    Console.Write("Digite a nova data de nascimento: ");
-                    string dataNasci = Console.ReadLine();
-                    this.DataNasci = dataNasci;
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida!");
-                    break;
-            }
-        }
-
-
-        public void Listar()
-        {
-            Console.WriteLine("Dados do cliente: " + this.Nome + ", " + this.Ende + ", " + this.DataNasci);
-        }
+        List<Cliente> listCli = new List<Cliente>();
 
         public void Excluir()
         {
-            Console.Write("Por favor, digite qual dado deseja excluir: ");
-            string opcao = Console.ReadLine();
+            Console.Write("Informe o cliente que deseja excluir: ");
+            this.Nome = Console.ReadLine();
 
-            switch (opcao)
-            {
-                case "nome":
-                    this.Nome = "";
-                    break;
-                case "endereço":
-                    this.Ende = "";
-                    break;
-                case "data de nascimento":
-                    this.DataNasci = "";
-                    break;
-                default:
-                    Console.WriteLine("Opção inválida!");
-                    break;
-            }
+            //listCli.Remove(this.Nome);
+
         }
 
     }
--- Atividade2_POO/Program.cs
+++ Atividade2_POO/Program.cs
@@ -10,46 +10,174 @@ namespace Atividade2_POO
     {
         static void Main(string[] args)
         {
-            Cliente c1 = new Cliente(1, "Yago", "Rua", "11/04/2000");
-            
+            int opcao = 0;
+            List<Cliente> clienteList = new List<Cliente>();
+            List<Cliente> novosCLientes = new List<Cliente>();
+            Console.WriteLine("Área do Cliente");
+            do
+            {
+                Console.WriteLine("Selecione a opção desejada");
+                Console.WriteLine("1 - Cadastro");
+                Console.WriteLine("2 - Editar");
+                Console.WriteLine("3 - Listar");
+                Console.WriteLine("4 - Excluir");
+                Console.WriteLine("5 - Sair");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+                switch (opcao)
+                {
+                    case 1:
+                        int proximoId = 0;
+                        if (clienteList.Count > 0)
+                        {
+                            int ultimoCliente = clienteList.Count();
+                            proximoId = clienteList[ultimoCliente].Id + 1;
+                        }
+                        novosCLientes = Cadastro(proximoId);
+                        clienteList.AddRange(novosCLientes);
+                        break;
 
-            
-            c1.Listar();
-            c1.Editar();
-            c1.Listar();
+                    case 2:
+                        Editar(clienteList);
+                        break;
 
-            c1.Excluir();
-            c1.Listar();
+                    case 3:
+                        Listar(clienteList);
+                        break;
 
-            //Colocar dentro de um método:
-            //Console.WriteLine("Olá! Por favor, selecione uma opção!");
-            //Console.WriteLine("-----------------");
-            //Console.WriteLine("| 1- Cliente    |");
-            //Console.WriteLine("| 2- Produto    |");
-            //Console.WriteLine("-----------------");
-            //int opcao = int.Parse(Console.ReadLine());
+                    case 4:
+                        Excluir(clienteList);
+                        break;
 
-            //switch (opcao)
-            //{
-            //    case 1:
-            //        Cliente.Menu();
-            //        break;
-            //    //case 2:
-            //    //    break;
-            //    //case 3:
-            //    //    break;
-            //    //case 4:
-            //    //    break;
-            //    default:
-            //        Console.WriteLine("Ooção inválida!");
-            //        break;
-            //}
 
-            //List<Cliente> lista = new List<Cliente>();
-            
+                    default:
+                        Console.WriteLine("opção inválida");
+                        break;
+                }
+            } while (opcao != 5);
+            Console.ReadLine();
+
+        }
+
+        static List<Cliente> Cadastro(int posicaoId)
+        {
+            List<Cliente> novosCLientes = new List<Cliente>();
+            bool verificacao = true;
+            string novoCadastro = "N";
+            Console.WriteLine("CADASTRO DE CLIENTE");
+            do
+            {
+                Cliente cliente = new Cliente();
+                cliente.Id = posicaoId;
+                Console.WriteLine("Por favor, nos informe os seguintes dados:");
+                Console.Write("Nome: ");
+                cliente.Nome = Console.ReadLine();
+                Console.Write("Endereço: ");
+                cliente.Endereco = Console.ReadLine();
+                Console.Write("Data de nascimento: ");
+                cliente.DataNascimento = Console.ReadLine();
+
+                novosCLientes.Add(cliente);
+                Console.Write("Deseja realizar um novo cadastro? S/N ");
+                novoCadastro = Console.ReadLine();
+                if (novoCadastro.ToUpper() != "S") { verificacao = false; }
+                Console.WriteLine("\n");
+
+                posicaoId++;
+            } while (verificacao);
+
+            return novosCLientes;
+        }
+
+        static void Editar(List<Cliente> clienteList)
+        {
+            var cliente = new Cliente();
+            Console.WriteLine("digite o nome completo do cliente que gostaria de editar");
+            cliente.Nome = Console.ReadLine();
+
+            cliente = clienteList.Find(x => x.Nome == cliente.Nome);
+            if (cliente != null)
+            {
+                Console.WriteLine($"Nome: {cliente.Nome}");
+                Console.WriteLine($"Endereço: {cliente.Endereco}");
+                Console.WriteLine($"Data de nascimento: {cliente.DataNascimento}");
+                Console.WriteLine("\n");
+
+                Console.WriteLine("NOVOS DADOS");
+                Console.Write("Nome: ");
+                string Nome = Console.ReadLine();
+                Console.Write("Endereço: ");
+                string Endereco = Console.ReadLine();
+                Console.Write("Data de nascimento: ");
+                string DataNascimento = Console.ReadLine();
+
+                Console.WriteLine("Deseja alterar os dados do cliente? S/N");
+                var aprovar = Console.ReadLine();
+                Console.WriteLine("\n");
+
+                if (aprovar.ToUpper() == "S")
+                {
+                    cliente.Nome = Nome;
+                    cliente.Endereco = Endereco;
+                    cliente.DataNascimento = DataNascimento;
+
+                    Console.WriteLine("cliente alterado com sucesso");
+                    Console.WriteLine("\n");
+                }
+            }
+            else
+            {
+                Console.WriteLine("cliente não encontrado!!");
+                Console.WriteLine("\n");
+            }
 
             Console.ReadLine();
-            
+        }
+
+        static void Listar(List<Cliente> clienteList)
+        {
+            int posicao = 1;
+            foreach (Cliente cliente in clienteList)
+            {
+                Console.WriteLine($"{posicao} - {cliente.Nome}");
+                posicao++;
+            }
+            Console.WriteLine("\n");
+            Console.ReadLine();
+        }
+
+        static void Excluir(List<Cliente> clienteList)
+        {
+            var cliente = new Cliente();
+            Console.WriteLine("digite o nome completo do cliente que gostaria de excluir");
+            cliente.Nome = Console.ReadLine();
+
+            cliente = clienteList.Find(x => x.Nome == cliente.Nome);
+            if (cliente != null)
+            {
+                Console.WriteLine($"Nome: {cliente.Nome}");
+                Console.WriteLine($"Endereço: {cliente.Endereco}");
+                Console.WriteLine($"Data de nascimento: {cliente.DataNascimento}");
+                Console.WriteLine("\n");
+
+                Console.WriteLine("Deseja excluir esse cliente? S/N");
+                var aprovar = Console.ReadLine();
+                Console.WriteLine("\n");
+
+                if (aprovar.ToUpper() == "S")
+                {
+                    clienteList.Remove(cliente);
+
+                    Console.WriteLine("cliente excluido com sucesso");
+                    Console.WriteLine("\n");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("cliente não encontrado!!");
+                Console.WriteLine("\n");
+            }
         }
     }
 }
